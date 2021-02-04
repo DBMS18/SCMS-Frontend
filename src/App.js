c808926c7ca893765832ab09a7ee7ff34cf531be
NethminiJPU <61895366+NethminiJPU@users.noreply.github.com>
2021-02-04 22:08:33 +0530

    Create a Contact Form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Contact from './pages/Contact';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function App() {
   return (
+    <ChakraProvider>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -18,7 +21,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <Contact />
     </div>
+    </ChakraProvider>
   );
 }
 
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,58 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { Container,Input,InputGroup,InputLeftElement,Stack} from "@chakra-ui/react";
+import { Heading } from "@chakra-ui/react";
+import { Textarea } from "@chakra-ui/react";
+import {ArrowRightIcon,ViewIcon,PhoneIcon,EmailIcon} from "@chakra-ui/icons";
+import { Button } from "@chakra-ui/react";
+//import { Icon } from "@chakra-ui/react";
+//import { MdSettings } from "react-icons/md";
+
+
+const Contact = ()=>{
+    return(
+      <ChakraProvider>
+        <Container bg="white" w="80%" p={4} color="black" border="2px" borderColor="gray.300" marginTop="14" marginBottom="14" borderRadius="lg">
+        <Heading margin="10">Contact Us</Heading>
+        <Stack spacing={5}>
+
+            <InputGroup>
+                <InputLeftElement
+                    pointerEvents="none"
+                    children={<ViewIcon color="gray.300" />}
+                />
+                <Input type="text" placeholder="Your Name" focusBorderColor="#22543D"  />
+            </InputGroup>
+
+            <InputGroup>
+                <InputLeftElement
+                    pointerEvents="none"
+                    children={<EmailIcon color="gray.300" />}
+                />
+                <Input type="email" placeholder="E-Mail"  focusBorderColor="#22543D" />
+            </InputGroup>
+
+            <InputGroup>
+                <InputLeftElement
+                    pointerEvents="none"
+                    children={<PhoneIcon color="gray.300" />}
+                />
+                <Input type="tel" placeholder="Contact Number" focusBorderColor="#22543D"  />
+            </InputGroup>
+
+            <Textarea 
+                focusBorderColor="#22543D"
+                placeholder="Your Message/Suggesion/complain" />
+            <Button rightIcon={<ArrowRightIcon />} bgColor="#22543D" color="white" variant="solid" width="max-content" alignSelf="center">
+                Submit
+            </Button>
+        </Stack>
+        </Container>
+
+      </ChakraProvider>
+        
+      
+    )
+  }
+
+  export default Contact;
+  
