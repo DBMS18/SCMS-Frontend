a8b4d576ca073b553fa541a22595f7d5088dbb29
Avishka-Rathnavibushana <mailavishka@gmail.com>
2021-02-11 08:34:43 +0530

    sign in and sign up

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Header from '../components/Header/Header';
 //import Footer from '../components/Footer';
 import Home from './Home';
+import Signup from './Signup';
+import Signin from './Signin';
 import Store from './Store';
 import Contact from './Contact';
 import { Switch, Route, Redirect } from 'react-router-dom';
@@ -21,6 +23,8 @@ class Main extends Component {
               <Route path='/home' component={HomePage} />
               <Route exact path='/store' component={() => <Store />} />
               <Route exact path='/contact' component={() => <Contact />} />
+              <Route exact path='/signup' component={() => <Signup />} />
+              <Route exact path='/signin' component={() => <Signin />} />
               <Redirect to="/home" />
           </Switch>
         {/* <Footer /> */}
